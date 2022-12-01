d0e3af1879d891a567b8d478b4a97fb7c3c59fad
PAAE03211 <ext.alan.amosse@sncf.fr>
2022-12-01 17:25:58 +0100

    new version mostly images and miel text

--- src/app/miels/miels.component.ts
+++ src/app/miels/miels.component.ts
@@ -16,43 +16,43 @@ export class MielsComponent implements OnInit {
       texture: 'Liquide.',
       strength: 1,
       place: 'Saint André de Corcy - La Dombes, Ain',
-      price: 8
+      price: 10
     },
     {
       title: 'Lavande',
-      description: 'Le lavandin est planté pour obtenir de l’huile essentielle aux multiples vertus appréciées. Ce miel est gourmand, très aromatique et à tendance à cristalliser.',
+      description: 'La lavande est plantés pour obtenir de l’huile essentielle aux multiples vertus appréciées. Ce miel est gourmand, très aromatique et à tendance à cristalliser.',
       taste: 'Doux,floral, arôme fin et parfumé.',
       texture: 'Solide, cristallisation fine et onctueuse.',
-      strength: 3,
+      strength: 2,
       place: 'Drôme provençale',
       price: 10
     },
     {
       title: 'Printemps',
-      description: 'Ce miel est le résultat printemps d\'un doux qui a permis aux abeilles de butiner de nombreuses essences. Il est le produit principal des fleurs de cloza, aubépine, et autres fruitiers. D’un goût naturellement doux, il est idéal pour tartiner ou sucrer les boissons chaudes.',
+      description: 'Ce doux a permis aux abeilles de butiner de nombreuses essences. Le nectar de cloza, de l\'aubépine, de l\'érable et d\'arbres fruitiers composent ce miel atypique.',
       taste: 'Suave et fruité, discret mais tenace.',
-      texture: 'Douce, à granulation fine.',
+      texture: 'Douce, crémeux',
       strength: 2,
       place: 'Priay - Plaine de l\'Ain',
       price: 6
     },
     {
       title: 'Été',
-      description: 'Cristallisé - Tournesol & Fleurs d\'été',
-      taste: '',
-      texture: '',
+      description: 'Ce miel orangé, principalement composé du nectar de tournesol, vous amènera du soleil cet hiver. Il est le commagnon idéal des enfants de par sa texture et son goût léger.',
+      taste: 'Doux, forte sucrosité.',
+      texture: 'Cristalisé.',
       strength: 2,
       place: 'Priay - Plaine de l\'Ain',
-      price: 10
+      price: 8
     },
     {
       title: 'Sapin & Tilleul',
-      description: 'Ce miel est l\'association naturelle de deux miellées d\'une même zone, sur des coteaux avoisinants la moyenne montagne. Il possède les caractéristiques mentholées uniques au fleurs de Tilleul, ainsi que le résineu tiré du miellat de Sapin. Cette excrétion est produite par les pucerons.',
-      taste: 'Effluve de résine, mentholé.',
+      description: 'Issu de coteaux avoisinants la moyenne montagne, ce nectar n\'est pas prêt de se faire oulier. Caractérisé par son goût mentholé et résineux unique, il ravira les amateurs de miels.',
+      taste: 'Frais, mentholé, effluve de résine',
       texture: 'Cristallisation lente mais fine.',
       strength: 3,
       place: 'Sutrieu - Valromey, Ain',
-      price: 10
+      price: 8
     },
     {
       title: 'Châtaignier',
@@ -61,7 +61,7 @@ export class MielsComponent implements OnInit {
       texture: 'Liquide, cristallisation lente et grossière.',
       strength: 4,
       place: 'Sutrieu - Valromey, Ain',
-      price: 10
+      price: 8
     },
   ];
 
